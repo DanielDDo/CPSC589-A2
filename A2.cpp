e690853f89810b8f798c8105ded91af68251f022
dudo <dudo@csx.cs.ucalgary.ca>
2018-02-21 18:53:59 -0700

    First commit

---
+++ A2.cpp
@@ -0,0 +1,266 @@
+// to run g++ tut1.cpp -lGL -lGLU -lglfw
+// ./a.out
+
+// Older OpenGL
+
+#include <GLFW/glfw3.h>	// Windows
+#include <glm/glm.hpp>
+#include <GL/gl.h>	     // OpenGL
+#include <GL/glu.h>	  // utility library
+#include <iostream>
+#include <vector>
+using namespace std;
+
+GLFWwindow *window;       // pointer to GLFW window
+int w, h;                 // width and height values
+double mouseX, mouseY;    // hold the X and Y pos of the mouse
+vector<glm::vec2> cps;    // Control Point array
+vector<glm::vec2> bspline;// BSpline Point array
+vector<int> sks;          // Standard Knot Sequence
+vector<float> U;          // Normalize Standard Knot Sequence
+int selected = -1;        // hold the index of the point we've clicked. -1 means we dont have anything selected
+float selectDistance = 0.03f; // size of the point so it's easier to click it
+float delta_u = 0.001f;     // u step size
+
+int k = 3;  // Order (degree + 1)
+int m = 0;  // Number of control points
+
+
+void printVecArr(vector<glm::vec2> arr) {
+  for (int i = 0; i < arr.size(); i++) {
+    cout << "<" << arr[i].x << "," << arr[i].y << ">" << endl;
+  }
+  cout << endl;
+}
+void printArrint(vector<int> arr) {
+  cout << arr.size() << endl;
+  cout << "{";
+  for (int i = 0; i < arr.size()-1; i++) {
+    cout << arr[i] << ", ";
+  }
+  cout << arr[arr.size()-1] << "}" << endl;
+}
+
+void printArrfloat(vector<float> arr) {
+  cout << arr.size() << endl;
+  cout << "{";
+  for (int i = 0; i < arr.size()-1; i++) {
+    cout << arr[i] << ", ";
+  }
+  cout << arr[arr.size()-1] << "}" << endl;
+}
+
+void updateKnotSeq() {
+  vector<int> knots;
+  // calculate knot seq info
+  int numKnots = k + m + 1;   // total number of knots
+  int rKnots = numKnots - 2*k;  // remaining knots (standard knot has multiplicity at the knots)
+
+  for (int i = 0; i < k; i++) {
+    knots.push_back(0);
+  }
+
+  for (int i = 0; i < rKnots; i++) {
+    knots.push_back(i+1);
+  }
+
+  for (int i = 0; i < k; i++) {
+    if (rKnots + 1 < 1) {
+      knots.push_back(1);
+    } else {
+      knots.push_back(rKnots+1);
+    }
+  }
+
+  sks = knots;
+}
+
+void normalizeKnotSeq() {
+  vector<float> knots;
+
+  float uniStepSize = m - k + 2;
+  if (uniStepSize <= 0.0f) {
+    uniStepSize = 1.0f;
+  }
+
+  for (int i = 0; i < sks.size(); i++) {
+    knots.push_back(sks[i]/uniStepSize);
+  }
+
+  U = knots;
+}
+
+int delta(float u, int k, int m) {
+  for (int i = 0; i < m + k-1; i++) {
+    if (u >= U[i] && u < U[i+1]) {
+      return i;
+    }
+  }
+}
+
+glm::vec2 E_delta_1(float u, int k, int m) {
+  vector<glm::vec2> c;
+  int d = delta(u, k, m);
+  cout << d << endl;
+  for (int i = 0; i < k-1; i++) {
+    c.push_back(cps[d-i]);
+  }
+  for (int r = k; r < 2; r--) {
+    int i = d;
+    for (int s = 0; s < r-2; s++) {
+      float omega = (u-U[i])/(U[i+r-1]-U[i]);
+      c[s] = omega * c[s] + (1-omega) * c[s+1];
+      i = i-1;
+    }
+  }
+  return c[0];
+}
+
+void renderBSpline() {
+  vector<glm::vec2> curvepoints;
+  float u = 0.0f;
+  for (u = 0.0f; u < 1.0f; u+=delta_u){
+    glm::vec2 p = E_delta_1(u, k, m);
+    curvepoints.push_back(p);
+  }
+
+  bspline = curvepoints;
+}
+
+/*************************************
+************** RENDER ****************
+*************************************/
+void render() {
+  // OpenGL calls go here
+  glEnable(GL_DEPTH_TEST);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+
+  glMatrixMode(GL_PROJECTION);  // scene - how it gets projected onto the screen
+  glLoadIdentity();
+  glOrtho(-1, 1, -1, 1, -1, 1); // view volume is a cube, which goes from -1 to 1 in all directions
+  //gluPerspective(fov, aspect ratio, near plane, far plane);
+  //glFrustum can also be used over gluPerspective
+
+  //renderBSpline();
+
+  glPointSize(7);
+  glBegin(GL_POINTS); // GL_POINTS, GL_QUADS, GL_LINES
+  for(int i = 0; i < cps.size(); i++) {
+    glColor3f(1.0f, 1.0f, 1.0f);
+    if(i == selected) {
+      glColor3f(1.0f, 0.0f, 0.0f);
+    }
+    glVertex2f(cps[i].x, cps[i].y);
+  }
+  glEnd();
+
+  /*glBegin(GL_LINE_STRIP);
+  glColor3f(1.0f, 0.0f, 0.0f);
+  for(int i = 0; i < cps.size(); i++) {
+    glVertex2f(cps[i].x, cps[i].y);
+  }
+  glEnd();
+*/
+  glBegin(GL_LINE_STRIP);
+  glColor3f(1.0f, 1.0f, 1.0f);
+  for (int i = 0; i < bspline.size(); i++) {
+    glVertex2f(bspline[i].x, bspline[i].y);
+  }
+  glEnd();
+}
+
+
+/**********************************
+********* USER INPUT CODE *********
+**********************************/
+void keyboard(GLFWwindow *sender, int key, int scancode, int action, int mods) {
+  if (action == GLFW_PRESS) {
+    if (key == GLFW_KEY_ESCAPE) {
+      glfwSetWindowShouldClose(window, 1);
+    }
+    if (key == GLFW_KEY_A) {
+      renderBSpline();
+      printVecArr(bspline);
+    }
+    if (key == GLFW_KEY_S) {
+      printArrfloat(U);
+    }
+    if (key == GLFW_KEY_D) {
+      for (float u = 0.0f; u < 1.0f; u+=0.1f) {
+        cout <<"delta(" << u << ") = " << delta(u, k, m) << endl;
+      }
+    }
+    if (key == GLFW_KEY_F) {
+      printVecArr(cps);
+    }
+  }
+
+}
+
+void mouseClick(GLFWwindow *sender, int button, int action, int mods) {
+  if (action == GLFW_PRESS) {
+    for(int i = 0; i < cps.size(); i++) {
+      if (abs(cps[i].x - mouseX) < selectDistance && abs(cps[i].y - mouseY) < selectDistance) {
+        selected = i;
+      }
+    }
+    if (button == GLFW_MOUSE_BUTTON_LEFT && selected == -1) {
+      cps.push_back(glm::vec2(mouseX, mouseY));
+    }
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && selected != -1) {
+      cps.erase(cps.begin() + selected);
+      selected = -1;
+    }
+    m = cps.size();
+    updateKnotSeq();
+    normalizeKnotSeq();
+  }
+  if (action == GLFW_RELEASE) {
+    selected = -1;
+  }
+}
+
+// mX and mY are in screen coordinates mX [0,width], mY [0, height] convert to [-1,1]
+void mousePos(GLFWwindow *sender, double mX, double mY) {
+  mouseX = (2 * mX / w) - 1;
+  mouseY = (-2 * mY / h) + 1; // want -1 at h, and 1 at 0.
+
+  if (selected != -1) {
+    cps[selected] = glm::vec2(mouseX, mouseY);
+  }
+}
+
+/********************************
+************ MAIN ***************
+********************************/
+int main() {
+  if(!glfwInit()) {
+    return 1;
+  }
+
+  window = glfwCreateWindow(640, 480, "Assignment 2 - CPSC587", NULL, NULL);  // width, height, window title
+  if(!window) { // check if window was created properly
+    return 1;
+  }
+
+  glfwMakeContextCurrent(window); // tell OpenGL what window we're working with
+  glfwSetKeyCallback(window, keyboard);
+  glfwSetMouseButtonCallback(window, mouseClick);
+  glfwSetCursorPosCallback(window, mousePos);
+
+  while(!glfwWindowShouldClose(window)) {
+    glfwGetFramebufferSize(window, &w, &h); // get attributes of the window size and store into w and h
+    glViewport(0, 0, w, h); // pixel coordinate of top left --> tell where to draw
+    render();
+
+    glfwSwapBuffers(window);  // double buffer -> work on 1 buffer, display the other
+    glfwPollEvents(); // detect input events --> mouse, keyboard. this detects when the mouse clicks the X in the top right
+  }
+
+  glfwDestroyWindow(window);  // destroy the window
+  glfwTerminate();
+  return 0;
+}
